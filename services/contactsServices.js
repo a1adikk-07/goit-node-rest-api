1694294e69440d5fca4a1c2f86bb5ce40d9cd14a
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2024-03-23 13:38:12 +0200

    commit

---
+++ services/contactsServices.js
@@ -0,0 +1,48 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.join("db", "contacts.json");
+
+export const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+};
+
+export const getContactById = async (contactId) => {
+  const data = await listContacts();
+  const contact = data.find((contact) => contact.id === contactId);
+  return contact || null;
+};
+
+export const addContact = async (data) => {
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  const contacts = await listContacts();
+  contacts.push(newContact);
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+export const removeContact = async (contactId) => {
+  const data = await listContacts();
+  const index = data.findIndex((item) => item.id === contactId);
+  if (index === -1) return null;
+  const [result] = data.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+  return result;
+};
+
+export const updContact = async (id, newData) => {
+  const contacts = await listContacts();
+
+  const index = contacts.findIndex((contact) => id === contact.id);
+  const currentContact = contacts[index];
+  if (index === -1) return null;
+  contacts[index] = { ...currentContact, ...newData };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+};
