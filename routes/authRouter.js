3991eae9a174d8ae46156b294ce8df321a9e802c
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2024-04-11 14:26:26 +0300

    commit

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createUserSchema, updateUserSchema } from "../models/user.js";
-import ctrl from "../controllers/authControllers.js";
+import { ctrl } from "../controllers/authControllers.js";
 import validateBody from "../middlewares/validateBody.js";
 import { authenticate } from "../middlewares/authenticate.js";
 
@@ -8,8 +8,8 @@ const authRouter = express.Router();
 
 authRouter.post("/register", validateBody(createUserSchema), ctrl.register);
 authRouter.post("/login", validateBody(createUserSchema), ctrl.login);
-authRouter.post("/logout", authenticate, ctrl.logout);
 authRouter.get("/current", authenticate, ctrl.getCurrent);
+authRouter.post("/logout", authenticate, ctrl.logout);
 authRouter.patch(
   "/",
   authenticate,
